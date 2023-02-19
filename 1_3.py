a1846cb17fcfcd3eaf2ac53430d89df792bb2bda
tatangracheva <grachtany@yandex.ru>
2023-02-19 15:01:17 +0300

    Выполнена домашняя работа 1

--- 1_3.py
+++ 1_3.py
@@ -1,13 +1,13 @@
 number = int(input('Введите номер билетика: '))
 part_1 = number//1000
 one = part_1//100
-two = part_1%100//10
-three = part_1%10
-part_2 = number%1000
+two = part_1 % 100//10
+three = part_1 % 10
+part_2 = number % 1000
 four = part_2//100
-five = part_2%100//10
-six = part_2%10
+five = part_2 % 100//10
+six = part_2 % 10
 if one + two + three == four + five + six:
     print('Билетик счастливый')
-else: print('Билетик не счастливый')
-
+else:
+    print('Билетик не счастливый')
